dbfc035ca065ca447a61672816b0ed237a946d14
ALAN <857599492@qq.com>
2019-04-01 17:46:04 +0800

    version:1.0

---
+++ js/public-js.js
@@ -0,0 +1,12 @@
+/**
+ *  version:1.0
+ *  creat by ALAN
+ *  github:https://github.com/AnselRain
+ */
+
+// 切换页面font-size值
+(function(){
+    var width = document.documentElement.clientWidth;
+    var html = document.querySelector("html");
+    html.style.fontSize=width/7.5+"px";
+})();
